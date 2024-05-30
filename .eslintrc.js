f319c23da0479f86f901ba4eb61f50a373f7df7a
CodePencil <jiawei.wang2021@gmail.com>
2024-05-30 17:54:19 +0800

    refactor: 调整了vue文件默认格式化插件以及eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   },
   rules: {
     'vue/multi-word-component-names': 'off',
-    'no-unused-vars': 'off'
+    'no-unused-vars': 'off',
+    'vue/require-v-for-key': 'off'
   }
 };
